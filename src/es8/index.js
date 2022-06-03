3b70eb9788445c84ff70e21956ac4b15b7e6b150
Francisco Escobar <franciscojescobar@gmail.com>
2022-06-03 18:35:56 -0500

    Ejemplos buenos de callback, funciones que llaman funciones simulando asincronia con setTimeout

--- src/es8/index.js
+++ src/es8/index.js
@@ -1,3 +1,4 @@
+// OBJECT.ENTRIES Y OBJECT.VALUES
 const datos = {
     frontend: 'Oscar',
     backend: 'Isabel',
@@ -22,8 +23,8 @@ console.log(array);
 console.log(array.length);
 
 
-console.log('hello'.padStart(7,'hi'));
-console.log('hello'.padEnd(12, ' ----L'));
+console.log('hello'.padStart(10,'hi '));
+console.log('hello'.padEnd(11, '*-----L'));
 console.log('food'.padEnd(12, ' ------' ))
 
 
@@ -31,4 +32,95 @@ const datos3 = {
     frontend: 'Oscar',  //TRAILING COMAS o sea comas finales al sigiente puede haber un valor o no
 }
 
+
+//FUNCIONES DE TIPO ASYNC CON FUNNCIONES TIPO AWAIT POR DENTRO
+const helloWorld = ()=> {
+    return new Promise( (resolve, reject)=> {
+        (true) // if ternario
+        ? setTimeout( ()=> resolve('Hello World, I am awesome:)'), 3000)
+        : reject( new Error('***** ERROR DE PRUEBA *****') );
+    })
+};
+
+const helloAsync = async ()=> {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsync();
+
+
+const anotherFunction = async ()=> {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
+
+//ESTO NO ES DE ES8 PERO ES UN APARTADO DE FRANCISCO ESCOBAR
+//PARA ENTENDER ASINCRONISMO EN JAVASCRIPT EMPEZANDO POR CALLBACKS
+
+function soyAsincrona() {
+    setTimeout(function() {
+            console.log("Hola, estoy siendo asincrona");
+    }, 1000);
+}
+
+console.log("Iniciando Proceso ...");
+soyAsincrona();
+console.log("Finalizando Proceso ...");
+
+
+
+//AHORA VAMOS A IMPLEMENTAR UN CALLBACK PARA QUE AVISE REALMENTE
+//CUANDO TERMINE EL PROCESO
+
+function soyAsincrona(miCallback) {
+    setTimeout(function() {
+            console.log("Hola, estoy siendo asincrona");
+            miCallback();
+    }, 1000);
+    
+}
+
+console.log("Iniciando Proceso ...");
+soyAsincrona(function() {
+     console.log("Terminando Proceso ...");
+    }
+);
+
+
+//AHORA LO QUE VAMOS A HACER ES LLAMAR UN CALLBACK
+//DENTRO DEL OTRO CALLBACK PARA DECIR HOLA FULANITO DE TAL Y ADIOS FULANITO DE TAL
+
+function hola(nombre, miCallback) {
+    setTimeout(function() {
+        console.log('Hola, ' + nombre);
+        miCallback(nombre);    
+    }, 2000);
+}
+
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Hasta luego ', nombre); //esta forma de concatenar es valida 
+        otroCallback();        
+    }, 1000);
+}
+
+
+console.log('Iniciando el proceso ...');
+
+hola('Carlos', function(nombre) {    
+    adios(nombre, function() {
+        console.log('Terminando el proceso ...');           
+    }); 
+});
+
+//hola('Francisco', ()=>{});
+//adios('Francisco', ()=>{});
 
